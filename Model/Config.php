6b1daf5691cf7771e3720c544e6dee4e7e62158a
Zaahed Yakubi <zaahed@vendic.nl>
2023-10-02 09:29:55 +0200

    Setting to enable or disable create account checkbox

--- Model/Config.php
+++ Model/Config.php
@@ -10,10 +10,19 @@ use Magento\Store\Model\ScopeInterface;
 
 class Config
 {
+    private const IS_ENABLED = 'hyva_themes_checkout/new_customer/enable';
     private const NEW_PASSWORD_TEMPLATE = 'hyva_themes_checkout/new_customer/create_password_template';
 
     public function __construct(private ScopeConfigInterface $scopeConfig)
     {
+    }
+
+    public function isEnabled(): bool
+    {
+        return $this->scopeConfig->getValue(
+            self::IS_ENABLED,
+            ScopeInterface::SCOPE_STORE
+        );
     }
 
     public function getNewPasswordTemplate(): string
